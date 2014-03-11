4f19a87123d8ba11b6ec548fe6da658838511d91
Daniel Mather <dmath979@mtroyal.ca>
2014-03-10 18:44:14 -0600

    Making changes to try and track location...failed but marking this code for posterity sake.

--- RoboticsTermProject/src/DetectedObjectListener.java
+++ RoboticsTermProject/src/DetectedObjectListener.java
@@ -1,31 +1,79 @@
+/*
+ * Author: Daniel Mather
+ * Description:
+ * This a test class that tries to map
+ */
+
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.objectdetection.Feature;
 import lejos.robotics.objectdetection.FeatureDetector;
 import lejos.robotics.objectdetection.FeatureListener;
 import lejos.hardware.lcd.LCD;
+import lejos.robotics.mapping.OccupancyGridMap;
  
 public class DetectedObjectListener implements FeatureListener
 {
     private DifferentialPilot pilot;
+    private OccupancyGridMap map;
+    private double bot_width;
+    private double bot_length;
+    
  
-    public DetectedObjectListener(final DifferentialPilot pilot)
+    public DetectedObjectListener(final DifferentialPilot pilot, 
+    		final OccupancyGridMap map, double bot_width, double bot_length)
     {
+    	// Give me local copies of things I need to use in this class
         this.pilot = pilot;
+        this.map = map;
+        this.bot_width = bot_width;
+        this.bot_length = bot_length;
     }
  
     @Override
-    public void featureDetected(final Feature feature, final FeatureDetector detector)
+    public void featureDetected(final Feature feature, 
+    		final FeatureDetector detector)
     {
         int range = (int)feature.getRangeReading().getRange();
-        if(range <= 45)
+        if(range <= 20)
         {
         	LCD.drawString("Distance: " + range, 0, 0);
-            if(range <= 20)
+            if(range <= 15)
             {
                 System.exit(0);
             }
+            double distance = Math.abs(pilot.getMovementIncrement() + bot_length*2);
+            LCD.drawString("Travel Dist: " + distance, 0, 3);
             pilot.stop();
-            pilot.rotate(300);
+            
+            // Iterate through the map...
+            for(int x = 0; x <= bot_width; x += 5)
+            {
+            	for(int y = 0; y <= map.getHeight(); y += 5)
+            	{
+            		if(y <= distance/5)
+            		{
+            			LCD.clear(4);
+            			map.setOccupied(x, y, 0);
+            			LCD.drawString("Not Occ: " + x + "," + y, 0, 4);
+            		}
+            		else
+            		{
+            			LCD.clear(5);
+            			map.setOccupied(x, (int)(distance/5)+1, 1);
+            			LCD.drawString("Occ" + x + "," + (int)(distance/5)+1,
+            					0, 5);
+            		}
+            	}
+            }
+            // Avoid obstacle...sorta.. (note this doesn't work for walls
+            // (obviously)
+            pilot.rotate(90);
+            pilot.travel(bot_length*1.5*-1);
+            pilot.rotate(-90);
+            pilot.travel(bot_length*1.5*-1);
+            pilot.rotate(-90);
+            pilot.travel(bot_length*1.5*-1);
+            pilot.rotate(90);
             pilot.backward();
         }
     }
--- RoboticsTermProject/src/test.java
+++ RoboticsTermProject/src/test.java
@@ -12,11 +12,14 @@ import lejos.hardware.port.Port;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.SensorModes;
+import lejos.robotics.Color;
 import lejos.robotics.navigation.DifferentialPilot;
 import lejos.robotics.RangeFinderAdaptor;
 import lejos.robotics.mapping.OccupancyGridMap;
 import lejos.robotics.objectdetection.RangeFeatureDetector;
 import lejos.robotics.objectdetection.FeatureListener;
+import lejos.robotics.objectdetection.Feature;
 
 public class test
 {
@@ -24,24 +27,44 @@ public class test
 	 * Initialize all constant variables here. All of these should be in CM or
 	 * CM/S.
 	 */
-	static final double TRACK_WIDTH = 16.5f;
-	static final double TRACK_LENGTH = 12;
-	static final int ARENA_LENGTH = 150;
-	static final int ARENA_WIDTH = 150;
+	// track width is distance between wheels
+	static final double TRACK_WIDTH = 142f;
+	// Track length is wheel diameter 
+	static final double TRACK_LENGTH = 30;
+	static final int ARENA_LENGTH = 1150;
+	static final int ARENA_WIDTH = 1150;
 	static final int ARENA_PRECISION = 5;
-	static final int MOVE_SPEED = 100;
+	static final int MOVE_SPEED = 50;
 	static final int MOTOR_ACCELERATION = 400;
-	static final int ROTATE_SPEED = 400;
+	static final int ROTATE_SPEED = 50;
+	static final int BOT_WIDTH = 200;
+	static final int BOT_LENGTH = 300;
+	static final int OCCUPIED_THRESHOLD = 1;
+	static final int FREE_THRESHOLD = 0;
 
 	public static void main(String[] args)
 	{
 		// We define the initial grid of the our arena (150x150cm)
 		// We define that the precision is 5cm
 		OccupancyGridMap map = new OccupancyGridMap(ARENA_LENGTH, ARENA_WIDTH,
-				20, 20, ARENA_PRECISION);
+				OCCUPIED_THRESHOLD, FREE_THRESHOLD, ARENA_PRECISION);
+		
+		// 0 is not occupied and 1 is occupied
+		// Mark the space that the bot occupies at the start as not occupied
+		for(int x = 0; x <= BOT_WIDTH; x +=5)
+		{
+			for(int y = 0; y <= BOT_LENGTH; y +=5)
+			{
+				map.setOccupied(x, y, 0);
+			}
+		}		
+		
 		// Instantiate a new differential pilot
+		// It should be noted that the differential pilot
+		// was not designed for use with tracked vehicles
 		DifferentialPilot pilot = new DifferentialPilot(TRACK_LENGTH,
 				TRACK_WIDTH, Motor.B, Motor.C, true);
+		
 		// Create our basic IR sensor for range keeping purposes
 		EV3IRSensor range = new EV3IRSensor(SensorPort.S2);
 
@@ -54,9 +77,10 @@ public class test
 		// distance
 		// This class is tripped when...
 		RangeFeatureDetector features = new RangeFeatureDetector(rangeAdapter,
-				100.0f, 500);
+				100.0f, 400);
 		
-		FeatureListener listener = new DetectedObjectListener(pilot);
+		FeatureListener listener = new DetectedObjectListener(pilot, map, 
+				BOT_WIDTH, BOT_LENGTH);
 		// Add a lister to the feature detector
 		features.addListener(listener);
 
