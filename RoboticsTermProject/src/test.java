4ef4bb8b3f33246cf3fbede9ede435af2e92f934
Daniel Mather <dmath979@mtroyal.ca>
2014-04-05 14:29:27 -0600

    Some crazy but terrible mapping code, I'm going to call it shadow mapping.

--- RoboticsTermProject/src/test.java
+++ RoboticsTermProject/src/test.java
@@ -25,6 +25,7 @@ import lejos.hardware.sensor.HiTechnicCompass;
 import lejos.robotics.DirectionFinder;
 import lejos.robotics.DirectionFinderAdaptor;
 import java.util.HashMap;
+import java.lang.Math;
 
 public class test
 {
@@ -50,10 +51,19 @@ public class test
 	static final int BOT_LENGTH = 30;
 	static final int OCCUPIED_THRESHOLD = 1;
 	static final int FREE_THRESHOLD = 0;
-	// Hashmap for rotatin angles
+	// Hash map for rotation angles
 	static HashMap<Integer, Double> rotation; 
 	static Thread mapper;
 	static private double heading;
+	// Offset is negative because the sensor is reading too far
+	static final double RANGE_SENSOR_OFFSET = -4.25;
+	// Readings around 90 are just too flaky, we also have to account for the
+	// offset in this as well.
+	static final double SENSOR_MAX_RANGE = 80 + RANGE_SENSOR_OFFSET;
+	// Bot origin is the bots center point in respect to origin (0,0)
+	// this is in x, y
+	static final double[] BOT_ORIGIN = {BOT_WIDTH/2, BOT_LENGTH/2};
+	static double[] POS = BOT_ORIGIN;
 
 	public static void main(String[] args)
 	{
@@ -61,19 +71,6 @@ public class test
 		// We define that the precision is 5cm
 		OccupancyGridMap map = new OccupancyGridMap(ARENA_LENGTH, ARENA_WIDTH,
 				OCCUPIED_THRESHOLD, FREE_THRESHOLD, ARENA_PRECISION);
-		
-		// Print the height of the map
-		LCD.drawString("Height: " + map.getHeight(), 0, 1);
-		
-		// 0 is not occupied and 1 is occupied
-		// Mark the space that the bot occupies at the start as not occupied
-		for(int x = 0; x <= BOT_WIDTH; x +=5)
-		{
-			for(int y = 0; y <= BOT_LENGTH; y +=5)
-			{
-				map.setOccupied(x, y, 1);
-			}
-		}		
 		
 		rotation = new HashMap<Integer, Double>();
 		// We're gonna have use this 90 to map out our iterations or 90
@@ -94,6 +91,101 @@ public class test
 		// Set up the range adapter using the IR sensors distance mode
 		RangeFinderAdaptor rangeAdapter = new RangeFinderAdaptor(
 				range.getDistanceMode());
+		
+		// Initialize these values so they can be used in the loop
+		int cur_x = -1;
+		int cur_y = -1;
+		double obj_range = -1;
+		int fixed_travel_amount = 5;
+		
+		// This loop will move forward (or in the height attribute for our map)
+		for(double distance_moved = 0; distance_moved + BOT_LENGTH + 5 < map.getHeight(); distance_moved += fixed_travel_amount)
+		{
+			LCD.drawString("Dist Moved: " + distance_moved, 0, 2);
+			cur_x =  (int)POS[0]/5;
+			cur_y = (int)POS[1]/5;
+			obj_range = convert_dm_to_cm(rangeAdapter.getRange()) + RANGE_SENSOR_OFFSET;
+			if(obj_range < SENSOR_MAX_RANGE)
+			{
+				LCD.drawString("Range: " + obj_range, 0, 4);
+				LCD.drawString("POS: " + POS[0] + "," + POS[1], 0, 3);
+				// Assume increments are in 5cm
+				for(int i = cur_x + (int)obj_range/5; i<map.getWidth()/5; i+=5)
+				{
+					try
+					{
+						// Sleep for half a second
+						Thread.sleep(500);
+					}
+					catch(InterruptedException e)
+					{
+						return;
+					}
+					LCD.clear(5);
+					map.setOccupied(i, cur_y, 1);
+					LCD.drawString("Pos: " + i + "," + cur_y + " occupid", 0, 5);
+				}
+			}
+			pilot.travel(convert_cm_to_mm(-fixed_travel_amount));
+			moved_y(fixed_travel_amount);
+			LCD.clear(4);
+			LCD.clear(3);
+			LCD.clear(2);
+		}
+		
+		// 
+		pilot.rotate(rotation.get(90)*-1);
+		
+		// This loop will move forward (or in the height attribute for our map)
+				for(double distance_moved = 0; distance_moved + BOT_WIDTH + 15 < map.getWidth(); distance_moved += fixed_travel_amount)
+				{
+					LCD.drawString("Dist Moved: " + distance_moved, 0, 2);
+					cur_x =  (int)POS[0]/5;
+					cur_y = (int)POS[1]/5;
+					obj_range = convert_dm_to_cm(rangeAdapter.getRange()) + RANGE_SENSOR_OFFSET;
+					if(obj_range < SENSOR_MAX_RANGE)
+					{
+						LCD.drawString("Range: " + obj_range, 0, 4);
+						LCD.drawString("POS: " + cur_x + "," + cur_y, 0, 3);
+						// Assume increments are in 5cm
+						for(int i = cur_y + (int)obj_range/5; i<map.getHeight()/5; i+=5)
+						{
+							try
+							{
+								// Sleep for half a second
+								Thread.sleep(500);
+							}
+							catch(InterruptedException e)
+							{
+								return;
+							}
+							LCD.clear(5);
+							map.setOccupied(cur_x, i, 1);
+							LCD.drawString("Pos: " + i + "," + cur_y + " occupid", 0, 5);
+						}
+					}
+					pilot.travel(convert_cm_to_mm(-fixed_travel_amount));
+					moved_x(fixed_travel_amount);
+					LCD.clear(4);
+					LCD.clear(3);
+					LCD.clear(2);
+				}
+		
+		range.close();
+		
+		// Need return so we don't run other code...
+		if(Button.waitForAnyPress() > 0)
+		{
+			return;
+		}
+			
+		if(obj_range < SENSOR_MAX_RANGE)
+		{
+			LCD.drawString("Range: " + obj_range, 0, 6);
+			Button.waitForAnyPress();
+			range.close();
+			return;
+		}
 		
 		//while(true)
 		//{
@@ -167,6 +259,7 @@ public class test
 		// Set zero to be going forward (all directions now reference this
 		// as being zero).
 		dir.resetCartesianZero();
+		set_direction(0);
 		
 		// Units are in cm
 		for(int i = 0; i<15; i++)
@@ -174,7 +267,7 @@ public class test
 			LCD.clear(6);
 			pilot.travel(convert_cm_to_mm(-15));
 			float rot = 90;
-			set_direction(rot);
+			//set_direction(rot);
 			pilot.rotate(rotation.get(90));
 			// Make any corrections
 			correct_angle(pilot, dir, rot);
@@ -187,8 +280,6 @@ public class test
 		//pilot.travel(convert_cm_to_mm(-1));
 		//pilot.rotate(-1*rotation.get(90));
 		
-		
-		
 		Button.waitForAnyPress();
 		
 		// close the sensors that we're using
@@ -196,23 +287,40 @@ public class test
 		range.close();
 	}
 	
+	public static void moved_x(double amount)
+	{
+		POS[0] = BOT_ORIGIN[0] + amount;
+	}
+	
+	public static void moved_y(double amount)
+	{
+		POS[1] = BOT_ORIGIN[1] + amount;
+	}
+	
+	public static double[] get_cur_pos()
+	{
+		return POS;
+	}
+	
 	// A method that will be able to be used to make course corrections
 	public static void correct_angle(DifferentialPilot pilot,
 			DirectionFinderAdaptor dir, double angle)
 	{
 		pilot.setRotateSpeed(400);
-		double t_direc = get_direction()+angle;
+		double t_direc = Math.abs(get_direction()+angle);
 		LCD.drawString("Dir: " + t_direc, 0, 4);
 		
 		double cur_direc = dir.getDegreesCartesian();
 		
 		while(cur_direc < t_direc-5 || cur_direc > t_direc+5)
 		{
+			LCD.clear(4);
 			// Try and correct itself in small increments, it may go all the way around in
 			// a circle.
 			// rotation is a hashmap that contains an approximate value for 90 degrees
 			pilot.rotate(rotation.get(90)/18);
 			cur_direc = dir.getDegreesCartesian();
+			LCD.drawString("Dir: " + t_direc, 0, 4);
 		}
 		set_direction(dir.getDegreesCartesian());
 	}
@@ -262,5 +370,24 @@ public class test
 		return cm*10;
 	}
 	
+	public static double convert_dm_to_cm(double dm)
+	{
+		// Centimeters
+		return dm*10;
+	}
 	
+	public static int[] get_grid_location(RangeFinderAdaptor range,
+			DifferentialPilot pilot)
+	{
+		int[] grid_pos = new int[2];
+		double distance_y = range.getRange();
+		// Rotate 90 degrees
+		pilot.rotate(rotation.get(90));
+		double distance_x = range.getRange();
+		
+		// Make my grid position
+		grid_pos[0] = (int)distance_x/5;
+		grid_pos[1] = (int)distance_y/5;
+		return grid_pos;
+	}
 }
